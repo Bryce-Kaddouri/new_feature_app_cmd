48de60254de55135c0653b6418c92cbcb9b416b4
Bryce-Kaddouri <bryce@MyCloudPAs-MacBook-Pro.local>
2023-12-12 12:16:14 +0000

    structure with content for each file added

--- main.py
+++ main.py
@@ -35,6 +35,9 @@ def camel_to_snake(word):
 def snake_to_camel(word):
     return ''.join(x.capitalize() or '_' for x in word.split('_'))
 
+def snakeToCamelLower(word):
+    return word[0].lower() + snakeToCamel(word)[1:]
+
 
 
 
@@ -70,14 +73,98 @@ def main():
             name = input('Enter the name of the feature (use the same format: FeatureName) : ')
             print('name: ', name)
             snake_name = camel_to_snake(name)
+            lower_camel_name = snake_to_camel(snake_name)
             path = 'lib/src/features/' + snake_name
             lst = path.split('/')
             print(lst)
             for i in range(1, len(lst)):
                 if not check_exists('/'.join(lst[:i])):
                     create_item('/'.join(lst[:i]))
-            """ create_item(path, is_folder=False, content='''import 'package:flutter/material.dart';''')
-            print('Feature created successfully') """
+            create_item(path, is_folder=True)
+            create_item(path + '/presentation', is_folder=True)
+            ''' provider '''
+            create_item(path + '/presentation/provider', is_folder=True)
+            create_item(path + '/presentation/provider/' + snake_name + '.dart', is_folder=False, content='''
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ''' + name + '''Provider with ChangeNotifier {
+    ''' + name + '''Provider();
+}
+''')
+
+            create_item(path + '/presentation/screen', is_folder=True)
+            create_item(path + '/presentation/screen/' + snake_name + '_screen.dart', is_folder=False, content='''
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+class ''' + name + '''Screen extends StatelessWidget {
+    const ''' + name + '''Screen({Key? key}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+        return Placeholder();
+    }
+}
+''')
+
+            create_item(path + '/presentation/widget', is_folder=True)
+
+            create_item(path + '/data', is_folder=True)
+            create_item(path + '/data/datasource', is_folder=True)
+            create_item(path + '/data/datasource/' + snake_name + '_datasource.dart', is_folder=False, content='''
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ''' + name + '''DataSource {
+    final SharedPreferences? sharedPreferences;
+    final DioClient? dioClient;
+    const ''' + name + '''DataSource({required this.sharedPreferences, required this.dioClient});
+}
+''')
+
+            create_item(path + '/data/model', is_folder=True)
+            create_item(path + '/data/model/' + snake_name + '_model.dart', is_folder=False, content='''
+class ''' + name + '''Model {
+    int? id;
+    ''' + name + '''Model({this.id});
+
+    ''' + name + '''Model.fromJson(Map<String, dynamic> json){
+        id = json['id'];
+    }
+
+    Map<String, dynamic> toJson() {
+        final Map<String, dynamic> data = <String, dynamic>{};
+        data['id'] = id;
+        return data;
+    }
+}
+''')
+
+            create_item(path + '/data/repository', is_folder=True)
+            create_item(path + '/data/repository/' + snake_name + '_repository_impl.dart', is_folder=False, content='''
+import '../../business/repository/''' + snake_name + '''_repository.dart';
+import '../datasource/''' + snake_name + '''_datasource.dart';
+
+
+class ''' + name + '''RepositoryImpl implements ''' + name + '''Repository {
+    final ''' + name + '''DataSource ''' + '''dataSource;
+    const ''' + name + '''RepositoryImpl({required this.dataSource});
+}
+''')
+
+
+            create_item(path + '/business', is_folder=True)
+            create_item(path + '/business/param', is_folder=True)
+
+            create_item(path + '/business/repository', is_folder=True)
+            create_item(path + '/business/repository/' + snake_name + '_repository.dart', is_folder=False, content='''
+abstract class ''' + name + '''Repository {
+
+}''')
+
+            create_item(path + '/business/usecase', is_folder=True)
+
+            print('Feature created successfully')
 
 
         elif choice == '2':
